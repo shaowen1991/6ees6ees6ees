861cb17f87adf0bcf09019f4b75714d63911f5dd
Hack Reactor Student <student@hackreactor.com>
2017-04-04 19:47:24 -0700

    update es6 on 6ee6ee6ees

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,10 @@
 class Grub {
+  constructor(age = 0, color = 'pink',food='jelly'){
+    this.color = color
+    this.age = age
+    this.food = food
+  }
+  eat (food) {
+    return 'Mmmmmmmmm ' + this.food
+  }
 };
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,16 @@
-class HoneyMakerBee {
+class HoneyMakerBee extends Bee{
+  constructor() {
+    super()
+    this.job = 'make honey'
+    this.honeyPot = 0
+    this.age = 10
+  }
+  makeHoney() {
+    this.honeyPot += 1
+  }
+
+  giveHoney() {
+    this.honeyPot -= 1
+  }
   // TODO..
 };
